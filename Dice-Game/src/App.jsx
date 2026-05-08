d9108666ef89e4f6bc50b48fbb825750536e63c3
prabin <bhusal1008@gmail.com>
2026-05-08 09:05:33 +0545

    learning to use styled-components for better and simple development process

--- Dice-Game/src/App.jsx
+++ Dice-Game/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import styled from "styled-components"
 
 function App() {
 
   return (
     <>
-      
-    
+      <Button> styled with styled-component </Button>
     </>
   )
 }
 
 export default App
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px;
+  border-radius: 3px;
+  border: 1px solid red;
+`
